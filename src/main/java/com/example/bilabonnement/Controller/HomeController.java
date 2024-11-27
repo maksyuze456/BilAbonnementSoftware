252220e5d9825c80b286781bd3be47f988be238c
Maksym <maksyuze456@gmail.com>
2024-11-27 20:15:17 +0100

    test af db

--- src/main/java/com/example/bilabonnement/Controller/HomeController.java
+++ src/main/java/com/example/bilabonnement/Controller/HomeController.java
@@ -1,8 +1,16 @@
 package com.example.bilabonnement.Controller;
 
+import com.example.bilabonnement.Repository.TestRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class HomeController {
-
-
+@Autowired
+    TestRepo testRepo;
+    @GetMapping("/")
+        public void test() {
+        testRepo.opretTabel();
+    }
 }
